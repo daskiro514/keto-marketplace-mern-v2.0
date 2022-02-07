08f7bf04fbb02db456ee8f0adf174f87f475149d
daskiro514 <luckybuzstar@gmail.com>
2022-02-08 01:46:01 +0900

    updated

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -13,7 +13,7 @@ const upload = multer({
     fileSize: 100000000 // max file size 1MB = 1000000 bytes
   },
   fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpeg|jpg|JPG|PNG|png|pdf|PDF|doc|docx|xlsx|xls)$/)) {
+    if (!file.originalname.match(/\.(jpeg|jpg|JPG|PNG|png|WEBP|webp|webP|pdf|PDF|doc|docx|xlsx|xls)$/)) {
       return cb(new Error('only upload files with jpg, jpeg, png, pdf, doc, docx, xslx, xls format.'))
     }
     cb(undefined, true) // continue with upload
